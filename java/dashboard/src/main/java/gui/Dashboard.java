ecb6aa946c8dec52b151d268049e4d4026782eac
Игорь Федоренко <ifedorenko@gmail.com>
2020-05-25 22:25:07 +0200

    - Добавил указание порта через аргументы командной строки

--- java/dashboard/src/main/java/gui/Dashboard.java
+++ java/dashboard/src/main/java/gui/Dashboard.java
@@ -6,16 +6,19 @@ import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.*;
 import reader.DataReader;
 
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Dashboard extends Application {
-
     private static final int SENSOR_REFRESH_DELAY = 500;
     private static final int SENSOR_REFRESH_INTERVAL = 20;
+
     private SensorSet sensorSet = new SensorSet();
 
     private Timer timer = new Timer();
@@ -54,17 +57,47 @@ public class Dashboard extends Application {
     }
 
     public static void main(String[] args) {
-        for (SerialPort s: SerialPort.getCommPorts()) {
-            System.out.println(s.getSystemPortName());
+        CommandLine cmd = parseOptions(args);
+        if (cmd != null) {
+            connectToComPort(cmd.getOptionValue("port"));
+            launch(args);
+        } else {
+            System.exit(-1);
+        }
+    }
+
+    private static CommandLine parseOptions(String ... args) {
+        Options options = new Options();
+
+        Option portOption = new Option("p", "port", true, "port");
+        portOption.setRequired(true);
+        options.addOption(portOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            log.error("Options error: ", e);
+            formatter.printHelp("dashboard", options);
+            log.info("Possible com ports: {}", Arrays
+                    .stream(SerialPort.getCommPorts())
+                    .map(SerialPort::getSystemPortName)
+                    .collect(Collectors.toList()));
         }
 
-        port = SerialPort.getCommPort("tty.usbmodem14101");
+        return null;
+    }
+
+    private static void connectToComPort(String portName) {
+        port = SerialPort.getCommPort(portName);
         port.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
-        port.openPort();
-
-        DataReader.getInstance().getReader().setSerialPort(port);
-        DataReader.getInstance().start();
-
-        launch(args);
+        if (port.openPort()) {
+            DataReader.getInstance().getReader().setSerialPort(port);
+            DataReader.getInstance().start();
+        } else {
+            throw new RuntimeException("Could not open given port " + portName);
+        }
     }
 }
